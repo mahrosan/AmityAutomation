d36ef1d8a67c36a36cd94b88fe59b5da495074ee
Noor Yeaser Khan <nyk5073@gmail.com>
2022-07-04 14:19:46 +0700

    removed hard coded chromedriver dependency to webdrivermanager

--- src/test/java/initializer/initializer.java
+++ src/test/java/initializer/initializer.java
@@ -1,5 +1,6 @@
 package initializer;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,8 +8,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class initializer {
 	public static WebDriver setUpChromeDriver() {
 		
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
-		
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
+		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
 		return new ChromeDriver(options);
